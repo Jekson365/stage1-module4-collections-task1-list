be3e097490dd9a78d538484e53bf767b42ef4c2d
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-28 11:32:29 +0400

    faa

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -8,13 +8,12 @@ public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
         ArrayList<String> finalArray = new ArrayList<String>();
 
-        for (int i = 1;i < sourceList.toArray().length;i++) {
-            if (i % 3 == 0) {
+        for (int i = 0;i < sourceList.toArray().length;i++) {
+            if ((i+1) % 3 == 0) {
                 finalArray.add(sourceList.get(i));
                 finalArray.add(sourceList.get(i));
             }
         }
-
         return finalArray;
     }
 }
